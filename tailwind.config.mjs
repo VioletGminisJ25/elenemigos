e6436db0d2bfc9a589192cc8d3d0d43066218915
VioletGminisJ25 <javierrodal25@gmail.com>
2024-05-07 10:42:28 +0200

    Adding querys declaration in configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			// Aquí se pueden poner como en un json unas claves que hagan referencia a una url o imagen o incluso reglas css para tener más completado con tailwind
+		},
+		screens: {
+			tablet: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			laptop: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			desktop: "1280px",
+			// => @media (min-width: 1280px) { ... }
+		},
 	},
 	plugins: [],
-}
+};
