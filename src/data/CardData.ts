dab7e260fadc7831cdf9c56b9916f1dc381412a3
VioletGminisJ25 <javierrodal25@gmail.com>
2024-09-07 01:32:49 +0200

    using astro components with props and no json and weird scripts

---
+++ src/data/CardData.ts
@@ -0,0 +1,47 @@
+import type { Card } from "../types/types";
+
+const Cards: Card[] = [
+	{
+		name: "Ghost of Tshushima",
+		image: "'/images/ghost-tsushima.webp'",
+		alt: "Ghost of Tshushima cover",
+		frase: "'El samurái deshonrado'",
+		sinopsis:
+			"El juego se remonta al año 1274 en la isla nipona de Tsushima, en plena invasión mongola. Tras fracasar en la defensa y ser uni de los pococs superviviente del ataque enemigo, Jin Sakai deberá dejar de lado su honor como samurái y enfrentarse al ejército mongol para liberar su hogar.",
+		puntosPos: "Historia muy buena e inmersiva, muy buenos graficos",
+		puntosNeg: "Pesies hasta resulta predecible.",
+		valoraApart:
+			"Historia: 9'5\nJugabilidad/Gameplay:7'8\nSonidos:9\nBanda Sonora:10\nGráficos:10",
+		valoraFinal:
+            "Ghost Of Tsushima es una apuesta muy acertada por parte de la desarrolladora Suker Punch.Pese a pecar en algunos aspectos del gameplay, el resto del juego lo compensa, dando como resultado en jugo imprescindible si eres jugador de PlayStation.",
+	},
+	{
+		name: "Toem",
+		image: "/images/toem.jpg",
+		alt: "Ghost of Tshushima cover",
+		frase: "'El samurái deshonrado'",
+		sinopsis:
+			"El juego se remonta al año 1274 en la isla nipona de Tsushima, en plena invasión mongola. Tras fracasar en la defensa y ser uni de los pococs superviviente del ataque enemigo, Jin Sakai deberá dejar de lado su honor como samurái y enfrentarse al ejército mongol para liberar su hogar.",
+		puntosPos: "Historia muy buena e inmersiva, muy buenos graficos",
+		puntosNeg: "Pesies hasta resulta predecible.",
+		valoraApart:
+			"Historia: 9'5\nJugabilidad/Gameplay:7'8\nSonidos:9\nBanda Sonora:10\nGráficos:10",
+		valoraFinal:
+			"Ghost Of Tsushima es una apuesta muy acertada por parte de la desarrolladora Suker Punch.Pese a pecar en algunos aspectos del gameplay, el resto del juego lo compensa, dando como resultado en jugo imprescindible si eres jugador de PlayStation.",
+	},
+	{
+		name: "Devil May Cry",
+		image: "/images/devil.webp",
+		alt: "Devil cover",
+		frase: "'El samurái deshonrado'",
+		sinopsis:
+			"El juego se remonta al año 1274 en la isla nipona de Tsushima, en plena invasión mongola. Tras fracasar en la defensa y ser uni de los pococs superviviente del ataque enemigo, Jin Sakai deberá dejar de lado su honor como samurái y enfrentarse al ejército mongol para liberar su hogar.",
+		puntosPos: "Historia muy buena e inmersiva, muy buenos graficos",
+		puntosNeg: "Pesies hasta resulta predecible.",
+		valoraApart:
+			"Historia: 9'5\nJugabilidad/Gameplay:7'8\nSonidos:9\nBanda Sonora:10\nGráficos:10",
+		valoraFinal:
+			"Ghost Of Tsushima es una apuesta muy acertada por parte de la desarrolladora Suker Punch.Pese a pecar en algunos aspectos del gameplay, el resto del juego lo compensa, dando como resultado en jugo imprescindible si eres jugador de PlayStation.",
+	},
+];
+export default Cards;
---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+
+export interface Card {
+	image: string;
+	name: string;
+	alt: string;
+	frase: string;
+	sinopsis: string;
+	puntosPos: string;
+	puntosNeg: string;
+	valoraApart: string;
+	valoraFinal:string;
+}
