1440251d239b9a0c280cacf4d3040f90414ae966
VioletGminisJ25 <javierrodal25@gmail.com>
2024-09-08 17:59:09 +0200

    solving vercel

---
+++ .vercel/output/functions/_render.func/home/javi/elenemigos2.0/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_DCTcoDUV.mjs';
+import { manifest } from './manifest_Cvk2FMYL.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "baf31f26-97dd-4dcb-8ecb-1634577f1dbd",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
